69441e977c2fa03e60fc0c5ee0478db56db625c4
m-masoumi <m-masoumi@agri-bank.com>
2024-02-17 13:00:16 +0330

    circle list

--- lib/circle_list_menu_page.dart
+++ lib/circle_list_menu_page.dart
@@ -1,6 +1,6 @@
-
 import 'package:circle_list/circle_list.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class CircleListMenuPage extends StatefulWidget {
   const CircleListMenuPage({super.key});
@@ -11,7 +11,7 @@ class CircleListMenuPage extends StatefulWidget {
 
 class _CircleListMenuPageState extends State<CircleListMenuPage> {
 
-  List<String> imgPaths= [
+  List<String> items= [
     'assets/images/apple2.png',
     'assets/images/beach.jpeg',
     'assets/images/card_bg.jpg',
@@ -22,27 +22,51 @@ class _CircleListMenuPageState extends State<CircleListMenuPage> {
     'assets/images/photo_eraser.png',
   ];
 
+
+  int currentIndex = 0;
+
+  final double radius = 200.0; // Radius of the circle
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         width: size.width,
         height: size.height,
         child:Center(
           child: CircleList(
-
+             rotateMode: RotateMode.allRotate,
             origin: Offset(0, 0),
-            children: imgPaths.map((imgPaths){
+            showInitialAnimation: false,
+            outerCircleColor: Colors.red,
+            innerCircleColor: Colors.greenAccent,
+
+            // onDragUpdate: (updateCoord) {
+            //    var a = 5;
+            // },
+
+            children: items.asMap().entries.map((entry){
+
+              final index = entry.key;
+              final item = entry.value;
 
               return GestureDetector(
                 onTap: (){ },
-                child: Container(
-                  width: 70,
-                    height:70,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        child: Image.asset(imgPaths , fit: BoxFit.fill,))),
+                  child: Container(
+                    width: 70,
+                      height:70,
+                      child: Stack(
+                        children: [
+                          ClipRRect(
+                              borderRadius: BorderRadius.all(Radius.circular(50)),
+                              child: Container(color: Colors.blueGrey,),
+                              // child: Image.asset(item , fit: BoxFit.fill,)
+                          ),
+
+                          Center(child: Text("Item"))
+                        ],
+                      )),
               );
             }).toList(),
             centerWidget: GestureDetector(
@@ -54,6 +78,14 @@ class _CircleListMenuPageState extends State<CircleListMenuPage> {
           ),
         ),
       ),
+
     );
   }
 }
+
+
+
+
+
+
+
